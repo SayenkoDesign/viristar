4b0d84b855cba43b7d51b9c954081cccc8ba1238
Kyle <kylerumble@gmail.com>
2025-01-03 13:23:59 -0800

    update home course finder

--- wp-content/themes/sayenko-viristar/blocks/find-a-course/index.php
+++ wp-content/themes/sayenko-viristar/blocks/find-a-course/index.php
@@ -14,7 +14,13 @@
 
 use \App\ACF_Block;
 
+$block_classes[] = 'alignfull section-padding-bottom';
+
 $block = new ACF_Block($block, $content, $is_preview);
+
+$block->add_render_attribute([
+    'class' => $block_classes,
+]);
 
 if ($is_preview) {
     echo '<h3>Find a Course</h3>';
@@ -32,10 +38,20 @@ $content = ob_get_clean();
 // Check if both title and content exist before outputting
 if (!empty($content)) {
     echo $block->before_render();
+    // Add the background here
+    // Add the wave here
+    printf('<div class="wave"><img src="%s/s-wave-shape-flipped.svg" /></div>', THEME_IMG);
+
+    echo '<div class="wrapper">';
+    echo '<div class="content-width">';
+    echo '<h2 class="section-title">Find a Course</h2>';
     get_template_part('template-parts/course-filters');
     echo '<div class="courses courses--grid">';
     get_template_part('template-parts/course-view');
     echo $content;
+    echo '</div>';
+    echo '<p class="view-all"><a class="gb-button" href="/education/courses/find-a-course/">View All Courses</a></p>';
+    echo '</div>';
     echo '</div>';
     echo $block->after_render();
 }
